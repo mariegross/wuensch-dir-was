23002267bdc0acdcc867e6e240a5d5e2b8c97f58
mariegross <marie.gross@gmx.de>
2020-11-05 10:21:27 +0100

    Add new script and concurrently

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 export async function getLists() {
-  const response = await fetch('http://localhost:3000/lists');
+  const response = await fetch('http://localhost:5000/lists');
   const lists = response.json();
   return lists;
 }
 
 export async function getListbyId(id) {
-  const response = await fetch(`http://localhost:3000/lists/${id}`);
+  const response = await fetch(`http://localhost:5000/lists/${id}`);
   const list = response.json();
   return list;
 }
